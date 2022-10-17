97ac2a63ff27dfaf0c5cf7e55a48416f84e033d2
skmm6 <egor.qaq@gmail.com>
2022-10-17 21:33:11 +0300

    171020222133

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -33,7 +33,7 @@
 
 // smash(1);
 // TODO задача 1
-
+/*
 const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 14, 9, 5];
 
 const calcTempAmplitude = function (temps) {
@@ -78,3 +78,53 @@ const calcTempAmplitudeNew = function (t1, t2) {
 
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
+*/
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'cels',
+
+    // C) FIX
+    // value: Number(prompt('Degrees celsius: ')),
+    value: 10,
+  };
+
+  // B) FIND
+  console.table(measurement);
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+//  A) identyfy
+console.log(measureKelvin());
+
+// USing a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const array1 = ['a', 'b', 'c'];
+  const array2 = ['d', 'e', 'f'];
+  const array3 = array1.concat(array2);
+
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+//  A) identyfy
+console.log(amplitudeBug);
