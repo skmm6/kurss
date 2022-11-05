25430b679746a204fbd100629ee587b79a7159ff
skmm6 <egor.qaq@gmail.com>
2022-11-05 18:01:53 +0300

    05.11.2022.18.01

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,6 +1,9 @@
 'use strict';
 
 //  Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -11,30 +14,83 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playning;
 //  Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diseEl.classList.add('hidden');
 
-let currentScore = 0;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playning = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  diseEl.classList.add('hidden');
+};
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // Rolling dice functionality
-
 btnRoll.addEventListener('click', function () {
-  // 1. Generality a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  if (playning) {
+    // 1. Generality a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // 2. Display dice
-  diseEl.classList.remove('hidden');
-  diseEl.src = `dice-${dice}.png`;
+    // 2. Display dice
+    diseEl.classList.remove('hidden');
+    diseEl.src = `dice-${dice}.png`;
 
-  // 3. Check for rolled 1
-  if (dice !== 1) {
-    // Add dice to current score
-    currentScore += dice;
-    current0El.textContent = currentScore; // CHAGE LATTER
-  } else {
-    // switch to next player
+    // 3. Check for rolled 1
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // switch to next player
+      switchPlayer();
+    }
   }
 });
+
+btnHold.addEventListener('click', function () {
+  if (playning) {
+    // 1. ADD current score to active player's score
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    //  2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+      // Finish game
+      playning = false;
+      diseEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // Swith to the next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2022 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 2000 && birthYear <= 2010) {
+      var zumer = true;
+      // Creating NEw variable with same name as outer scopes variable
+      const firstName = 'Steven';
+
+      //  переназначение внешних перменных
+      output = 'NEW OUTPUT!';
+
+      const str = `Oh, and you're a zumer, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(zumer);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  //   console.log(str);
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Egor';
+calcAge(2000);
+// console.log(age);
+// printAge();
