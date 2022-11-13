216358ac34aad81ad42c9ef904dac63c4e323769
skmm6 <egor.qaq@gmail.com>
2022-11-13 22:12:10 +0300

    13.11.2022

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -45,6 +45,78 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+// Логичский оператор объеденения
+/*
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+// OR оператор присваивания
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+//  оператор нулевого присваивания ( null или undefind)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND оператор присваивания
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+// операторы
+
+// restaurant.numGuests = 0;
+const guest = restaurant.numGuests || 10;
+console.log(guest);
+
+// Нулевые значения: null и undefined (не 0 или '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+
+console.log('--------- OR ----------');
+// Любой тип данных, возвращать любой тип данных - оценка коротного замыкания (если первое значение истенное оно вернет первое значение)
+// оператор или
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 0;
+const guest1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guest1);
+
+const guest2 = restaurant.numGuests || 10;
+console.log(guest2);
+
+console.log('--------- AND ----------');
+// оператор и
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+console.log('hello' && 23 && null && 'Jonas');
+
+//  Практическое задание
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+//
 
 // 1) Деструктурированние
 // шаблон rest
@@ -83,7 +155,7 @@ add(...x);
 restaurant.orderPizza('mushrooms', 'onion', 'spinach');
 restaurant.orderPizza('mushrooms');
 
-/*
+
 ////////////////////////////////////////////////////////////
 // оператор распространения
 const arr = [7, 8, 9];
