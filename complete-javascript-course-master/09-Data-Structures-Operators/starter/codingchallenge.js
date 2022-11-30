494632ebc42eead4617ad062fde8b6f90660d3fa
skmm6 <egor.qaq@gmail.com>
2022-11-30 20:59:17 +0300

    30.11.2022.20.59

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/codingchallenge.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/codingchallenge.js
@@ -1,46 +1,121 @@
 'use strict';
 /*
 // coding challenge #1
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+// КОДИНГ ЧЕЛЕНДЖ 3 ===================================
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '� Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '� Substitution'],
+  [64, '� Yellow card'],
+  [69, '� Red card'],
+  [70, '� Substitution'],
+  [72, '� Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '� Yellow card'],
+]);
+
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+// 3
+console.log(`Событие происходило каждые ${90 / gameEvents.size} минут`);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(`Событие происходило каждые ${time / gameEvents.size} минут`);
+
+// 4
+for (const [time, event] of gameEvents)
+  console.log(
+    `${time <= 45 ? '[ПЕРВЫЙ ТАЙМ]' : '[ВТОРОЙ ТАЙМ]'} ${time}: ${event}`
+  );
+// КОДИНГ ЧЕЛЕНДЖ 3 ===================================
+
+
+// for (const name of game.scored) {
+//   console.log(`Гол ${i + 1}: ${name}`);
+// }
+// КОДИНГ ЧЕЛЕНДЖ 2 ===================================
+
+// 1.
+for (const [i, players] of game.scored.entries()) {
+  console.log(`Гол ${i + 1}: ${players}`);
+}
+// 2.
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) {
+  average += odd;
+}
+average /= odds.length;
+console.log(average);
+
+// 3.
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'ничью' : `победу ${game[team]}`;
+  console.log(`Коэффициент на ${teamStr}... ${odd} `);
+}
+// бонус
+const scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+  console.log(scorers);
+}
+// КОДИНГ ЧЕЛЕНДЖ 2 ===================================
+// const properties = Object.keys(game.scored);
+
+// for (const gol of properties) {
+//   console.log(`Гол ${+gol + 1}: ${game.scored[gol]}`);
+// }
+
+// for (let i = 0; i < game.scored.length; i++) {
+//   console.log(`Гол ${i + 1}: ${game.scored[i]}`);
+// }
+
 
 // 1)
 const [players1, players2] = game.players;
--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,110 @@
 'use strict';
 
+// СТРОКИ  PART 1==============================TODO
+/*
+const airLine = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airLine.length);
+console.log('B737'.length);
+
+console.log(airLine.indexOf('r'));
+console.log(airLine.lastIndexOf('r'));
+console.log(airLine.indexOf('portugal'));
+
+console.log(airLine.slice(4));
+console.log(airLine.slice(4, 7));
+
+console.log(airLine.slice(0, airLine.indexOf(' ')));
+console.log(airLine.slice(airLine.lastIndexOf(' ') + 1));
+
+console.log(airLine.slice(-2));
+console.log(airLine.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E средние места
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E')
+    console.log('Твоем место по середине оно не нужно никому 😒');
+  else console.log('Это хорошее место😊');
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('jonas'));
+console.log(typeof new String('jonas'));
+
+console.log(typeof new String('jonas').slice(1));
+*/
+// СТРОКИ  PART 2==============================TODO
+const airLine = 'TAP Air Portugal';
+
+console.log(airLine.toLowerCase());
+console.log(airLine.toUpperCase());
+
+// Заглавная буква
+const passenger = 'jOnAS'; // Jonas
+const passengerLower = passenger.toLocaleLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+//  Проверь электронную почку (сравнение)
+
+const email = 'hello@jonas.io';
+const loginEmail = '  Hello@Jonas.io \n';
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+
+const normalizedEmail = loginEmail.toLocaleLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// replacing
+const priceGB = '228,97$';
+const priceUS = priceGB.replace('$', 'R').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'Все пассажиры подходят к выходу на посадку 23. выходу на посадку 23!';
+
+console.log(announcement.replace('выходу', 'входу'));
+console.log(announcement.replaceAll('выходу', 'входу'));
+
+console.log(announcement.replaceAll(/выходу/g, 'входу'));
+
+// Bolleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Air'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Крч успешно прошло условие');
+}
+
+// Практический опыт
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+};
+checkBaggage('I have a laptop, some Food and pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Get some snacks and a gun for protection');
+
+/*
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
 const openingHours = {
@@ -47,7 +152,175 @@ const restaurant = {
   },
 };
 
-/*
+
+
+
+//MAPS итерации TODO
+const question = new Map([
+  ['question', 'Какой самый лучший язык программирования в мире?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+// Фактически преобразуем объект в картуTODO
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Приложение викторинаTODO
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+const answer = 3;
+console.log(question.get(question.get('correct') === answer));
+
+// Преобразуем карту в массив
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+// MAP TODO 
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed :( ');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+
+// МНОЖЕТВА TODO
+
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+console.log(ordersSet);
+
+console.log(new Set('Jonas'));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+// ordersSet.clear();
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+// Пример  TODO
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+);
+
+console.log(new Set('jonasschmedtmann').size);
+
+
+// Пербор циклы не итеративныйTODO
+
+// Название объектовTODO
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `Мы открыты ${properties.length} дня:`;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+
+console.log(openStr);
+// Значение объекта
+const values = Object.values(openingHours);
+console.log(values);
+
+// Зацыкливание на объектье
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// [key, value]
+for (const [day, { open, close }] of entries) {
+  console.log(`В ${day} откроется в ${open} и закроется в ${close}`);
+}
+
+
+// Опциональная цепочка
+if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon.open);
+
+//дополнительная цепочка
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// методы
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Массивы;
+
+const users = [
+  {
+    name: 'Jonas',
+    email: 'hello@jonas.io',
+  },
+];
+
+// const users = [];
+
+console.log(users[0]?.name ?? 'User array empty');
+if (users.length > 0) console.log(users[0].name);
+else console.log('User array empty');
+
+
+
 // Цикл for of для переборки массива 
 
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
