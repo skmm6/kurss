be7e8efc78652519175f01f45d9e79926bf90915
skmm6 <egor.qaq@gmail.com>
2023-01-03 05:20:33 +0300

    02.01.2023

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -26,7 +26,7 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
-*/
+
 
 const flight = 'LH234';
 const jonas = {
@@ -61,3 +61,97 @@ newPassport(jonas);
 checkIn(flight, jonas);
 
 
+const oneWorld = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Функция высокого порядка
+const transformer = function (str, fn) {
+  console.log(`Оригинальная: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Трансформировал при помощи функции: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWorld);
+
+// Js обратные вызовы происходят постоянно
+const high5 = function () {
+  console.log('✋');
+};
+
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+const greetHey = greet('Hey');
+greetHey('Jonas');
+greetHey('Steven');
+
+greet('Hello')('Jonas');
+
+const greetSt = greeting => name => console.log(`${greeting} ${name}`);
+
+greetSt('Hellows')('Egor');
+*/
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function(){}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(635, 'John Smith');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// не работает
+// book(23, 'Sarag Williams');
+
+// Вызови метод Call
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Copper');
+
+// Apply метод
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+
+book.call(swiss, ...flightData);
+console.log(swiss);
