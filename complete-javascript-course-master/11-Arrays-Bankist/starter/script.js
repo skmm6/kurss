c930e2694903823c22b1294919d2e99016392758
skmm6 <egor.qaq@gmail.com>
2023-02-07 22:38:23 +0300

    07.02.2023 2238

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -347,7 +347,7 @@ const avr1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avr2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 
 console.log(avr1, avr2);
-*/
+
 const eurToUsd = 1.1;
 
 // PIPELINE
@@ -360,3 +360,38 @@ const totalDepositsUSD = movements
   // .map(mov => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
+
+
+///////////////////////////////////////////
+//Coding Challenge #2
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+const avr1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avr2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avr1, avr2);
+
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+// const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+
+const account = function (accs) {
+  for (const acc of accs) {
+    if (acc.owner === 'Jessica Davis') {
+      return acc;
+    }
+  }
+};
+
+console.log(account(accounts));
+*/
