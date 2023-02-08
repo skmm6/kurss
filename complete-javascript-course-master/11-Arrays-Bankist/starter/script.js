903bdf88af4966bf4deaf66b7c902cc16eb208ef
skmm6 <egor.qaq@gmail.com>
2023-02-08 22:59:11 +0300

    08.02.2023 2259

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -677,4 +677,75 @@ console.log(convertTitleCase('this is a LONG title but not too long'));
 console.log(convertTitleCase('and here is another title with an EXAMPLE'));
 */
 ///////////////////////////////
-/// Методы масивов практика
+/// 4 челендж
+
+// test data
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// 1.
+dogs.forEach(
+  dog => (dog.recommendedFood = Math.trunc(dog.weight ** 0.75 * 28))
+);
+// console.log(dogs);
+
+// 2.
+
+// мой
+// dogs.forEach((dog, i) => {
+//   const humn = dog.owners.forEach(hum => hum === 'Sarah' && console.log(dog));
+// });
+// его
+const dogSara = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(dogSara);
+console.log(
+  `Sarah's dog is eating ${
+    dogSara.curFood > dogSara.recommendedFood ? 'much' : 'little'
+  }`
+);
+// console.log(dogSara);
+
+// 3.
+
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.recommendedFood * 1.1 < dog.curFood)
+  .flatMap(dog => dog.owners);
+
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.recommendedFood * 0.9 > dog.curFood)
+  .flatMap(dog => dog.owners);
+
+console.log(ownersEatTooMuch, ownersEatTooLittle);
+
+// 4.
+// "Матильда и Собаки Алисы и Боба едят слишком много!"
+//  "Собаки Сары, Джона и Майкла едят слишком мало!"
+console.log(`${ownersEatTooMuch.join(' and ')} едят слишком много!`);
+console.log(`${ownersEatTooLittle.join(' and ')} едят слишком мало!`);
+
+// 5.
+console.log(dogs.some(dog => dog.curFood === dog.recommendedFood));
+
+// 6
+// current > (recommended * 0.90) && current < (recommended * 1.10)
+const checkEatingOkay = dog =>
+  dog.curFood > dog.recommendedFood * 0.9 &&
+  dog.curFood < dog.recommendedFood * 1.1;
+
+console.log(dogs.some(checkEatingOkay));
+
+// 7.
+const dogsOkay = dogs.filter(checkEatingOkay);
+console.log(dogsOkay);
+
+// 8.
+const dogsSorted = dogs
+  .slice()
+  .sort((a, b) => a.recommendedFood - b.recommendedFood);
+
+console.log(dogsSorted);
