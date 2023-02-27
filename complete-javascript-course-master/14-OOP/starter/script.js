dac5ff8028571392b08b5e18376fe69f893491ae
skmm6 <egor.qaq@gmail.com>
2023-02-27 16:24:12 +0300

    270223

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,58 @@
 'use strict';
+
+//////////////////////////////////////////////////
+//208 Constructor Functions and the new Operator TODOTODOTODOTODOTODOTODO
+//////////////////////////////////////////////////
+
+const Person = function (firstName, birthYear) {
+  // Свойства экземпляра
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //   Never to this плохая практика не делай так
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+//  1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function auto return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jack instanceof Person);
+
+//////////////////////////////////////////////////
+//209 Prototypes TODOTODOTODOTODOTODOTODO
+//////////////////////////////////////////////////
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jack.calcAge();
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOfLinkedObjects
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('spacies'));
