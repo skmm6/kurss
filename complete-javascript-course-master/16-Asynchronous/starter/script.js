59b7f3aa88deea44b0ff156812bccd4be1b53608
Alacosta Alacosta <egor.qaq@gmail.com>
2023-03-07 23:16:35 +0300

    AJAX обратный вызов подводные

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -1,11 +1,12 @@
 'use strict';
 
+///////////////////////////////////////
+//248 Welcome to Callback Hell TODO
+/////////////////////////////////////////
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+/*
 const getCountryData = function (country) {
-  const btn = document.querySelector('.btn-country');
-  const countriesContainer = document.querySelector('.countries');
-
-  ///////////////////////////////////////
-
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v2/name/${country}`);
   request.send();
@@ -37,3 +38,75 @@ getCountryData('portugal');
 getCountryData('usa');
 getCountryData('russia');
 getCountryData('belorus');
+*/
+///////////////////////////////////////
+//250 Welcome to Callback Hell TODO
+/////////////////////////////////////////
+
+const renderCountry = function (data, className = '') {
+  const html = `
+      <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Визуализировать 1 страну
+    renderCountry(data);
+
+    // соседняя страна (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+// getCountryAndNeighbour('portugal');
+getCountryAndNeighbour('usa');
+// getCountryAndNeighbour('russia');
+
+setTimeout(() => {
+  console.log('1 second');
+  setTimeout(() => {
+    console.log('2 second');
+    setTimeout(() => {
+      console.log('3 second');
+      setTimeout(() => {
+        console.log('4 second');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
