5ba88879278a8ae48922ab4a68619378629f884d
Alacosta Alacosta <egor.qaq@gmail.com>
2023-03-16 06:40:12 +0300

    next90 пет проект

--- complete-javascript-course-master/18-forkify/starter/src/js/controller.js
+++ complete-javascript-course-master/18-forkify/starter/src/js/controller.js
@@ -31,11 +31,15 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
     //1) Загружаем рецепт
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
       // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bccb2'
     );
     const data = await res.json();
@@ -160,4 +164,7 @@ const showRecipe = async function () {
     alert(err);
   }
 };
-showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
